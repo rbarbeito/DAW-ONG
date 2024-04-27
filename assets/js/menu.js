713297be13d0a4d24fdf2fa95f4b6ea298006cd2
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-04-27 15:24:55 +0200

    Correccion de errores en pagina de inicio

---
+++ assets/js/menu.js
@@ -0,0 +1,11 @@
+let openm = document.getElementById('openm')
+let closem = document.getElementById('closem')
+let nav = document.getElementById('navi')
+
+openm.addEventListener('click', () => {
+  nav.classList.add('visible')
+})
+
+closem.addEventListener('click', () => {
+  nav.classList.remove('visible')
+})
